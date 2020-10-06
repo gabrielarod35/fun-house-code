928f40783bbb1b5537a700d8461ad9518bdd2b17
Peter Bryant <45208929+peterwbryant@users.noreply.github.com>
2020-10-06 12:17:03 -0500

    Update main.py

--- DanduCanDo/main.py
+++ DanduCanDo/main.py
@@ -1,1 +1,28 @@
 # Placeholder for your code
+def main():
+  # get name
+  name = input("What is your hero's name? ")
+
+  # initialize points
+  points = 0
+  points = points + dp
+  
+  print("You are entering the fun house!")
+  print("welcome to the Ajays room of the funhouse,",name+"!")
+  print("lets get started!")
+
+  print("there are doors one to let you live and continue &.")
+  print("one with a raw cookie dough.\n")
+
+  print("They are called door 1 & 2")
+  print("you have to choose, but one door will let you continue and gain pts and the other one will give you salmonella.")
+  answer = input("Which door do you choose.")
+
+  if answer == "2":
+      print(" you get 10pts! and no salmonella!")
+      pts = +10
+
+  if answer == "1":
+      print("you get salmonella and lose 10 pts.")
+      pts = -10
+  print("Congrats you now get to move onto the next students room.")
