6e14908259e3eb27b47a1b7dbabda6199d67f1a6
gabrielarod35 <gabrielarod35@gmail.com>
2020-10-19 19:07:21 +0000

    added two

--- funhousegame.py
+++ funhousegame.py
@@ -497,5 +497,49 @@ def winnertext():
     return pts
 
 
+# gaby's room 
+def main(): 
+  name = input("What is your hero's name?")
+  
+  points = 0 
+  
+  dp  = Gaby's game 
+  points = points + dp 
+  print("Welcome ,"name+"!")
+  print("Your adventure begins")
+  print("There are 2 cups n the table, one blue and one red")
+  print("Be careful, a spider lives in one of them that will take you back to your previous roound -40 point")
+  print("The other cup has a code to advabce to the nect round")
+  input("Which cup do you choose? red or blue")
+  answer = input("Which cup do you choose? red or blue")
+  if answer == "red"
+    print("Better luck next time, -40 points")
+    points = -40 
+  if answer == "blue"
+    print("CONGRATS! You have discovered the code, +50 points")
+    points = +60
+    print("Advance to the next level")
+    return points 
 
 
+#eric's room
+Def main():
+  name = input("What is your heros name?")
+  points = 0
+  
+  dp = Eric`s room(name)
+  points = points + dp
+  print("good to have you in this game, ",name+"!")
+  print(" Let the game begin!!")
+  print("there are two buttons in this room one is red the other is blue")
+  print("watch out for the red button it can send you back to the previous room by -25 points")
+  print(" the blue button can instantly send you to the final stage of this game by +50 points")
+  input("which button do you press? red or blue")
+  if answer == "red":
+    print("unlucky, go back to the previous room, -25")
+    points = -25
+  if answer == "blue":
+    print(" great job dude, +50)
+    points = +50
+    print(" goodluck in your future endevors")
+    return points
